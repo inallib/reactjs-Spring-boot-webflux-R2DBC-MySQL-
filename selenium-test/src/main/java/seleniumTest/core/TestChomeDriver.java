375da7e9d02b7e2c547553fd74b462c2bc2a4e4a
inallib@gmail.com <billani1-1>
2020-12-14 00:34:56 +0530

    driver path change

--- selenium-test/src/main/java/seleniumTest/core/TestChomeDriver.java
+++ selenium-test/src/main/java/seleniumTest/core/TestChomeDriver.java
@@ -2,15 +2,23 @@ package main.java.seleniumTest.core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import sun.misc.ClassLoaderUtil;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
 
 public class TestChomeDriver {
     static WebDriver webDriver;
 
     public static WebDriver getWebDriver(){
         if (webDriver == null) {
+            ClassLoader classLoader = TestChomeDriver.class.getClassLoader();
+            File driverFile = new File(classLoader.getResource("chromedriver.exe").getFile());
+
             System.setProperty(
                     "webdriver.chrome.driver",
-                    "C:/NonSystems/Saurav/hackathons/Candela labs/selenium-test/src/main/resources/chromedriver.exe");
+                    driverFile.getPath());
             webDriver = new ChromeDriver();
             return webDriver;
         }
